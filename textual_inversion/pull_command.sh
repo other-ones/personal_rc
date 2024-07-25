f4a60829ba038740b0977eac6305c27ccdad4405
other-ones <tuk101234@gmail.com>
2024-07-25 18:56:50 -0400

    update

--- textual_inversion/pull_command.sh
+++ textual_inversion/pull_command.sh
@@ -1,3 +1,6 @@
+current_dir=$(pwd)
 cd ../;
 git pull origin main;
-cd pplus;
+cd "$current_dir"
+cp assets/modeling_clip.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py ;
+cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py ;
