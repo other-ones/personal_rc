4c29366672efcaaeed55dc2043a6c6703c031545
other-ones <tuk101234@gmail.com>
2024-07-25 16:40:13 -0400

    update

--- mlm/train_mlm.sh
+++ mlm/train_mlm.sh
@@ -1,11 +1,11 @@
 
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export MODEL_NAME="stabilityai/stable-diffusion-2-1";
-export CUDA_VISIBLE_DEVICES=2,3;
+export CUDA_VISIBLE_DEVICES=0,1;
 accelerate launch --num_processes=2 --main_process_port=7354 train_mlm.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir="saved_models" \
-  --train_batch_size=150 \
+  --train_batch_size=128 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --lr_scheduler="constant" \
@@ -18,11 +18,11 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm.py \
   --seed=7776 \
   --mask_tokens='[MASK]' \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
-  --run_name='mlm_contextnet_nonpad_lr1e4' \
+  --run_name='sd2_contextnet_nonpadding_1e4' \
   --mlm_weight=5 \
   --mlm_target='non_padding' \
   --whole_word_mask=0 \
-  --project_name='SD MLM' 
+  --project_name='ContextNet Train' 
 
 
 
