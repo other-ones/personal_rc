879f716edafb65e075b873a29c5539bd8abff320
other-ones <tuk101234@gmail.com>
2024-07-25 18:58:15 -0400

    update

--- textual_inversion/pull_command.sh
+++ textual_inversion/pull_command.sh
@@ -3,4 +3,4 @@ cd ../;
 git pull origin main;
 cd "$current_dir"
 cp assets/modeling_clip.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py ;
-cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py ;
+cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py ;a
