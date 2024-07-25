184b5406e785f516ac309fc907236fa12947e94c
other-ones <tuk101234@gmail.com>
2024-07-25 19:03:26 -0400

    update

--- dreambooth/pull_command.sh
+++ dreambooth/pull_command.sh
@@ -1,3 +1,3 @@
 git pull origin main
-cp assets/modeling_clip.py /home/twkim/anaconda3/envs/char/lib/python/site-packages/transformers/models/clip/modeling_clip.py
-cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/char/lib/python/site-packages/transformers/modeling_outputs.py
+cp assets/modeling_clip.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py
+cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py
