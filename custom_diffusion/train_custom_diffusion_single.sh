0acd7d5ecff042b5d2cb5bfef9e96424d6d68186
other-ones <tuk101234@gmail.com>
2024-07-31 22:02:55 -0400

    update

---
+++ custom_diffusion/train_custom_diffusion_single.sh
@@ -0,0 +1,41 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5";
+export OUTPUT_DIR="outputs";
+export INSTANCE_DIR="/data/twkim/diffusion/personalization/collected/images/pet_cat1";
+export CUDA_VISIBLE_DEVICES=6,7;
+accelerate launch train_custom_diffusion.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --class_data_dir=./priors/samples_cat/ \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --class_prompt="cat" --num_class_images=200 \
+  --instance_prompt="photo of a <new1> cat"  \
+  --resolution=512  \
+  --train_batch_size=4  \
+  --learning_rate=8e-5  \
+  --lr_warmup_steps=0 \
+  --max_train_steps=250 \
+  --scale_lr --hflip  \
+  --modifier_token "<new1>"
+  # --real_prior acc
+
+
+  export MODEL_NAME="runwayml/stable-diffusion-v1-5";
+export OUTPUT_DIR="outputs";
+export INSTANCE_DIR="/data/twkim/diffusion/personalization/collected/images/vase";
+export CUDA_VISIBLE_DEVICES=0;
+accelerate launch train_custom_diffusion.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --class_data_dir=./gen_reg/samples_vase/ \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --class_prompt="vase" --num_class_images=200 \
+  --instance_prompt="photo of a <new1> vase"  \
+  --resolution=512  \
+  --train_batch_size=8  \
+  --learning_rate=8e-5  \
+  --lr_warmup_steps=0 \
+  --max_train_steps=500 \
+  --scale_lr --hflip  \
+  --modifier_token "<new1>"
