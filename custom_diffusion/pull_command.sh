3dedce399c847f4de86256fa893aff072110cea1
other-ones <tuk101234@gmail.com>
2024-08-08 17:46:05 -0400

    update

--- custom_diffusion/pull_command.sh
+++ custom_diffusion/pull_command.sh
@@ -1,5 +1,5 @@
 cd ../;
 git pull origin main;
-cd DisenBooth;
+cd custom_diffusion;
 cp assets/modeling_clip.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py;
 cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py;
