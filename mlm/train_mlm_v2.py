7cefc151fa7cf525c6cc752d5ab0bbfafa43ddda
other-ones <tuk101234@gmail.com>
2024-08-20 13:42:02 -0400

    update

--- mlm/train_mlm_v2.py
+++ mlm/train_mlm_v2.py
@@ -102,6 +102,13 @@ def main():
         os.system('cp *.py {}'.format(codepath))
         os.system('cp datasets_pkgs {} -R'.format(codepath))
         os.system('cp packages {} -R'.format(codepath))
+        # copy clip
+        os.makedirs(os.path.join(codepath,'clip_src'),exist_ok=True)
+        target = os.readlink('clip_src/modeling_clip.py')
+        shutil.copy2(target, '{}/clip_src/modeling_clip.py'.format(codepath))
+        target = os.readlink('clip_src/modeling_outputs.py')
+        shutil.copy2(target, '{}/clip_src/modeling_outputs.py'.format(codepath))
+        # copy clip
         # 1. command
         command_path=os.path.join(codepath,'command.txt')
         command_file=open(command_path,'w')
