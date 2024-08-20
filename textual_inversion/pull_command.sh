6587ecd01961d763351dfca68442c1a0a4366cda
other-ones <tuk101234@gmail.com>
2024-08-20 06:33:54 -0400

    update

--- textual_inversion/pull_command.sh
+++ textual_inversion/pull_command.sh
@@ -5,5 +5,5 @@ cd "$current_dir"
 # cp assets/modeling_clip.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py;
 # cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py;
 
-ln -s /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py;
-ln -s /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py;
+# ln -s /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/models/clip/modeling_clip.py;
+# ln -s /home/twkim/anaconda3/envs/context/lib/python/site-packages/transformers/modeling_outputs.py;
