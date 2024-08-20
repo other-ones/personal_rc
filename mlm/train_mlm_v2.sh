14495cbff2efa1562e76999c3a9bb930111b4fad
other-ones <tuk101234@gmail.com>
2024-08-20 14:12:34 -0400

    update

--- mlm/train_mlm_v2.sh
+++ mlm/train_mlm_v2.sh
@@ -1,6 +1,9 @@
 
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export MODEL_NAME="stabilityai/stable-diffusion-2-1";
+
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export CUDA_VISIBLE_DEVICES=0,1;
 accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v2.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -30,7 +33,7 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v2.py \
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export MODEL_NAME="stabilityai/stable-diffusion-2-1";
 export CUDA_VISIBLE_DEVICES=2,3;
-accelerate launch --num_processes=2 --main_process_port=7353 train_mlm.py \
+accelerate launch --num_processes=2 --main_process_port=7353 train_mlm_v2.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir="saved_models/mlm_models" \
   --train_batch_size=128 \
@@ -39,7 +42,7 @@ accelerate launch --num_processes=2 --main_process_port=7353 train_mlm.py \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=100001 \
-  --save_steps=1000 \
+  --save_steps=10000 \
   --local_rank=0 \
   --visualize_steps=25 \
   --num_inference_steps=25 \
@@ -48,8 +51,9 @@ accelerate launch --num_processes=2 --main_process_port=7353 train_mlm.py \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
   --run_name='sd2_contextnetv2_all_1e4' \
   --mlm_weight=5 \
-  --mlm_target='all' \
+  --mlm_target='non_padding' \
   --whole_word_mask=0 \
+  --checkpoints_total_limit=10 \
   --report_to="wandb" \
   --project_name='ContextNet Train' 
 
