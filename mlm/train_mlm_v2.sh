2cf1fb1122aa27f7e2c785873e916b6a904401ae
other-ones <tuk101234@gmail.com>
2024-08-20 14:24:03 -0400

    update

--- mlm/train_mlm_v2.sh
+++ mlm/train_mlm_v2.sh
@@ -4,13 +4,13 @@ export MODEL_NAME="stabilityai/stable-diffusion-2-1";
 
 
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export CUDA_VISIBLE_DEVICES=0,1;
+export CUDA_VISIBLE_DEVICES=0,4;
 accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v2.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir="saved_models/mlm_models" \
   --train_batch_size=128 \
   --gradient_accumulation_steps=1 \
-  --learning_rate=1e-4 \
+  --learning_rate=5e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=100001 \
@@ -20,7 +20,7 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v2.py \
   --seed=7776 \
   --mask_tokens='[MASK]' \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
-  --run_name='sd1_contextnetv2_nonpadding_1e4' \
+  --run_name='sd1_contextnetv2_nonpadding_5e4' \
   --mlm_weight=5 \
   --mlm_target='non_padding' \
   --whole_word_mask=0 \
@@ -75,7 +75,7 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v2.py \
   --seed=7776 \
   --mask_tokens='[MASK]' \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
-  --run_name='sd2_contextnetv2_nonpadding_1e4' \
+  --run_name='sd2_contextnetv2_nonpadding_5e4' \
   --mlm_weight=5 \
   --mlm_target='non_padding' \
   --whole_word_mask=0 \
