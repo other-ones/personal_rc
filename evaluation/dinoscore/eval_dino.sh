91432ef77de1ea5ce7bc3f2277881a1fb26fc407
other-ones <tuk101234@gmail.com>
2024-08-22 14:02:21 -0400

    update

--- evaluation/dinoscore/eval_dino.sh
+++ evaluation/dinoscore/eval_dino.sh
@@ -4,8 +4,8 @@ python eval_dino.py \
 --keywords='single_seed2940' \
 --grounded=0
 
-export CUDA_VISIBLE_DEVICES=7;
+export CUDA_VISIBLE_DEVICES=0;
 python eval_dino.py \
 --method_root='./../../textual_inversion' \
---keywords='single_seed2940' \
+--keywords='singlev2_seed2940' \
 --grounded=0
