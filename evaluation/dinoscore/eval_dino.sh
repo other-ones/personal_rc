1057f1fe90946bc650c21f84784503e80f1a697d
other-ones <tuk101234@gmail.com>
2024-08-22 14:18:19 -0400

    update

--- evaluation/dinoscore/eval_dino.sh
+++ evaluation/dinoscore/eval_dino.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=1;
+export CUDA_VISIBLE_DEVICES=0;
 python eval_dino.py \
 --method_root='./../../dreambooth' \
---keywords='single_seed2940' \
+--keywords='singlev2_seed2940' \
 --grounded=0
 
 export CUDA_VISIBLE_DEVICES=0;
