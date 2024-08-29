2f3eb0ecf87536f78fa4071b2a95446f8db82606
other-ones <tuk101234@gmail.com>
2024-08-29 04:33:09 -0400

    update

--- mlm/train_mlm_v4.sh
+++ mlm/train_mlm_v4.sh
@@ -20,20 +20,21 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v4.py \
   --seed=7776 \
   --mask_tokens='[MASK]' \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
-  --run_name='sd1_contextnetv4_nonpadding_1e4_unnorm' \
+  --run_name='sd1_contextnetv4_nonpadding_1e4_unnorm_mprob015' \
   --mlm_weight=5 \
   --mlm_target='non_padding' \
   --whole_word_mask=0 \
   --checkpoints_total_limit=10 \
-  --report_to="wandb" \
+  --mask_prob=0.15 \
   --normalize_mask_embeds=0 \
+  --report_to="wandb" \
   --project_name='ContextNetV4 Train' 
 
 
 
 export MODEL_NAME="stabilityai/stable-diffusion-2-1";
-export CUDA_VISIBLE_DEVICES=6,7;
-accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v4.py \
+export CUDA_VISIBLE_DEVICES=4,5;
+accelerate launch --num_processes=2 --main_process_port=7353 train_mlm_v4.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir="saved_models/mlm_models" \
   --train_batch_size=128 \
@@ -48,12 +49,13 @@ accelerate launch --num_processes=2 --main_process_port=7354 train_mlm_v4.py \
   --seed=7776 \
   --mask_tokens='[MASK]' \
   --caption_path='/data/dataset/coco/karpathy/coco_caption_raw.txt' \
-  --run_name='sd2_contextnetv4_nonpadding_1e4_unnorm' \
+  --run_name='sd2_contextnetv4_nonpadding_1e4_unnorm_mprob015' \
   --mlm_weight=5 \
   --mlm_target='non_padding' \
   --whole_word_mask=0 \
   --normalize_mask_embeds=0 \
-  --checkpoints_total_limit=10 
+  --checkpoints_total_limit=10 \
+  --mask_prob=0.15 \
   --report_to="wandb" \
   --project_name='ContextNetV4 Train' 
 
