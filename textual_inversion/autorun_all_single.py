f46f892ad411a2514a7ac2dfbc0946d291a63560
other-ones <tuk101234@gmail.com>
2024-09-13 00:43:27 -0400

    update

--- textual_inversion/autorun_all_single.py
+++ textual_inversion/autorun_all_single.py
@@ -113,7 +113,12 @@ benchmark='dreambooth'
 mlm_target_list=['masked']
 nonmask_weight_list=[1]
 train_batch_size=1
-learning_rate='1e-3'
+if train_batch_size==4:
+    learning_rate='5e-4'
+elif train_batch_size==1:
+    learning_rate='1e-3'
+else:
+    assert False
 if include_prior:
     dir_name='single_batch{}_capv7_prior_seed{}_rep{}'.format(train_batch_size,seed,rep_id)
 else:
@@ -129,6 +134,7 @@ mlm_batch=25
 check_tags=['']
 # check_tags=['']
 for mask_prob in mask_prob_list:
+    continue
     for nonmask_weight in nonmask_weight_list:
         nonmask_weight_str=float_to_str(nonmask_weight)
         nonmask_weight_str=nonmask_weight_str.replace('.','')
