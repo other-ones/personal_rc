0ed236407245d400572b0e8af921b247c470684e
other-ones <tuk101234@gmail.com>
2024-09-13 01:39:46 -0400

    update

--- textual_inversion/ti_train.py
+++ textual_inversion/ti_train.py
@@ -768,22 +768,24 @@ def main():
                     mlm_logits=cls_net(clip_text_embedding_masked)
                     masked_idxs_flat=masked_idxs.view(-1)
                     nonmask_idxs_flat=~masked_idxs_flat
+                    mlm_labels_flat=mlm_labels.view(-1)
                     loss_mlm = F.cross_entropy(
                         mlm_logits.view(-1,cls_output_dim),
                         mlm_labels.view(-1),
                         # ignore_index=-100,
                         reduction='none'
                     )
-
-                    # masked_idxs_flat
-                    if args.nonmask_weight!=1 and args.mlm_target in ['non_special','all','non_padding']:
-                        loss_mlm[nonmask_idxs_flat]*=args.nonmask_weight
-                    elif args.mlm_target in ['masked']:
-                        # masked
-                        loss_mlm=loss_mlm[masked_idxs_flat]
-                    else:
-                        assert False
+                    loss_mlm=loss_mlm[mlm_labels_flat!=(-100)]
                     loss_mlm=loss_mlm.mean()
+                    # # masked_idxs_flat
+                    # if args.nonmask_weight!=1 and args.mlm_target in ['non_special','all','non_padding']:
+                    #     loss_mlm[nonmask_idxs_flat]*=args.nonmask_weight
+                    # elif args.mlm_target in ['masked']:
+                    #     # masked
+                    #     loss_mlm=loss_mlm[masked_idxs_flat]
+                    # else:
+                    #     assert False
+                    # loss_mlm=loss_mlm.mean()
                     loss=loss+(loss_mlm*args.lambda_mlm)
                     assert isinstance(mask_token_ids,list)
 
