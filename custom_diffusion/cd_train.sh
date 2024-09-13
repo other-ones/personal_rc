22d8421f517dfbf8ea1c7d2529c31bd5c18c2420
other-ones <tuk101234@gmail.com>
2024-09-12 22:25:23 -0400

    update

--- custom_diffusion/cd_train.sh
+++ custom_diffusion/cd_train.sh
@@ -35,8 +35,7 @@ accelerate launch --main_process_port=9980 cd_train.py \
   --seed=2940 \
   --mlm_batch_size=25 \
   --initializer_token="sks" \
-  --mask_prob=0.15 \
-  --initialize_token=1
+  --mask_prob=0.25 
 
 export MODEL_NAME="runwayml/stable-diffusion-v1-5";
 export OUTPUT_DIR="saved_models/cd_models/single/dog6";
--- evaluation/dinoscore/eval_dino_dir.sh
+++ evaluation/dinoscore/eval_dino_dir.sh
@@ -3,7 +3,7 @@ python eval_dino_dir.py \
 --dir_path='./../../dreambooth/results/db_results/single_capv7_seed2940_rep1' \
 --grounded=1 --ignore_legacy=1
 
-export CUDA_VISIBLE_DEVICES=1;
+export CUDA_VISIBLE_DEVICES=7;
 python eval_dino_dir.py \
---dir_path='./../../dreambooth/results/db_results/single_capv7_seed2940_rep1' \
---grounded=1 --ignore_legacy=0
+--dir_path='./../../dreambooth/results/db_results/single_noacc_seed7777_rep1' \
+--grounded=0 --ignore_legacy=0
