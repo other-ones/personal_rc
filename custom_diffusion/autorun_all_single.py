673940978b5fc28313fe3bfcaecdb8641701cc92
other-ones <tuk101234@gmail.com>
2024-09-17 13:59:00 -0400

    update

--- custom_diffusion/autorun_all_single.py
+++ custom_diffusion/autorun_all_single.py
@@ -8,24 +8,24 @@ print(hostname,'hostname')
 concepts=os.listdir('/data/twkim/diffusion/personalization/collected/images')
 info_map={
     # train_prior/eval_prior/train_prompt_type/eval_prompt_type
-    'backpack':('backpack','backpack','nonliving','nonliving'),
-    'cat_statue': ('toy','toy','nonliving','nonliving'),
-    'chair1': ('chair','chair','nonliving','nonliving'),
-    'cat1': ('cat','cat','pet','living'),
-    'dog6': ('dog','dog','pet','living'),
-    'duck_toy':('duck','duck toy','nonliving','nonliving'),
-
-    'dog3':  ('dog','dog','pet','living'),
-    'pet_cat1':('cat','cat','pet','living'),
-    'cat2':('cat','cat','pet','living'),
-
     'teapot':('teapot','teapot','nonliving','nonliving'),
-    'wooden_pot':('pot','wooden pot','nonliving','nonliving'),
-    'backpack_dog':('backpack','backpack','nonliving','nonliving'),
-    'poop_emoji':('toy','toy','nonliving','nonliving'),
-    'pet_dog1':('dog','dog','pet','living'),
-    'rc_car':('toy','toy','nonliving','nonliving'),
-    'teddybear':('teddy','teddy bear','nonliving','nonliving'),
+    # 'backpack':('backpack','backpack','nonliving','nonliving'),
+    # 'cat_statue': ('toy','toy','nonliving','nonliving'),
+    # 'chair1': ('chair','chair','nonliving','nonliving'),
+    # 'cat1': ('cat','cat','pet','living'),
+    # 'dog6': ('dog','dog','pet','living'),
+    # 'duck_toy':('duck','duck toy','nonliving','nonliving'),
+
+    # 'dog3':  ('dog','dog','pet','living'),
+    # 'pet_cat1':('cat','cat','pet','living'),
+    # 'cat2':('cat','cat','pet','living'),
+
+    # 'wooden_pot':('pot','wooden pot','nonliving','nonliving'),
+    # 'backpack_dog':('backpack','backpack','nonliving','nonliving'),
+    # 'poop_emoji':('toy','toy','nonliving','nonliving'),
+    # 'pet_dog1':('dog','dog','pet','living'),
+    # 'rc_car':('toy','toy','nonliving','nonliving'),
+    # 'teddybear':('teddy','teddy bear','nonliving','nonliving'),
 
     
     
