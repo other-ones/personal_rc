e1b806375bff1c6b2e078b46e1d077178c99d960
other-ones <tuk101234@gmail.com>
2024-09-19 16:12:24 -0400

    update

--- cd_partial/tmp.py
+++
@@ -1,3 +0,0 @@
-import os
-print(os.path.join(os.path.dirname(__file__),'../'))
-print(os.path.abspath(os.path.join(__file__,'../')))
--- cd_partial/vcheck.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0,'packages')
-import diffusers
-print(diffusers.__version__)
